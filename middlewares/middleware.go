52937001620e7790ef31315566231b40016bd9f6
Edilson Freitas <diillson@hotmail.com>
2024-10-13 14:49:58 -0300

    feat(security): force https.

--- middlewares/middleware.go
+++ middlewares/middleware.go
@@ -19,8 +19,8 @@ func ForceHTTPSMiddleware(next http.Handler) http.Handler {
 			return
 		}
 
-		// Verifica o cabeçalho X-Forwarded-Proto
-		if r.Header.Get("X-Forwarded-Proto") != "https" {
+		// Verifica o cabeçalho X-Forwarded-Proto e X-Forwarded-Ssl
+		if r.Header.Get("X-Forwarded-Proto") != "https" && r.Header.Get("X-Forwarded-Ssl") != "on" {
 			// Constrói a URL de redirecionamento para HTTPS
 			target := "https://" + r.Host + r.URL.RequestURI()
 			log.Printf("Redirecionando para HTTPS: %s", target)
